b68ab57a09686925f130ab409baa00a2ceaca723
Stephen Leung <fatstar415@gmail.com>
2022-07-15 02:01:21 +0800

    Make API call every 10s for latest data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import TrainPositionFilter from './TrainPositionFilter';
 export const AppContext = createContext(null);
 
 function App() {
+  const [intId, setIntId] = useState([]);
   const [trainPosition, setTrainPosition] = useState([]);
   const [lineCode, setLineCode] = useState('');
   const [serviceType, setServiceType] = useState('');
@@ -28,30 +29,53 @@ function App() {
     return filteredPositions;
   }
 
-  useEffect(() => {
-    setTrainPosition([]);
-    setIsLoaded(false);
+  function fetchTrainPosition() {
+    const dateTime = new Date();
+    console.log(`${dateTime.toISOString()} Calling API to fetch train position data.`);
+
     fetch("https://api.wmata.com/TrainPositions/TrainPositions?contentType=json", {
       method: "GET",
       headers: { api_key: '5bc33b9b23f148e88291c9aee432dbbd' },
       mode: "cors"
     })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          let filteredResult = filterTrainPosition(result.TrainPositions)
-          setIsLoaded(true);
-          setTrainPosition(filteredResult);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
+    .then(res => res.json())
+    .then(
+      (result) => {
+        let filteredResult = filterTrainPosition(result.TrainPositions)
+        setIsLoaded(true);
+        setTrainPosition(filteredResult);
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
+  }
+
+  useEffect(() => {
+    if (intId.length > 0) {
+      intId.map((int) => clearInterval(int));
+      fetchTrainPosition();
+    }
   }, [lineCode, serviceType, carCount]);
+
+  useEffect(() => {
+    if (trainPosition.length > 0) {
+      intId.map((int) => clearInterval(int));
+      let delayMS = 10000;
+      let intervalId = setInterval(fetchTrainPosition, delayMS);
+      setIntId([intervalId]);
+    }
+  }, [trainPosition]);
+
+  useEffect(() => {
+    if (trainPosition.length <= 0) {
+      fetchTrainPosition();
+    }
+  }, []);
 
   return (
     <div className="App">
